30fe563e4f954c1a0e75970e4437882e7fed2734
hiromi-mi <hiromi-mi@cat.zaq.jp>
2021-05-29 18:41:45 +0900

    implement get_rel_32

--- disassembler.py
+++ disassembler.py
@@ -2,6 +2,49 @@ from elftools.elf.elffile import ELFFile
 import elftools.elf.sections as sections
 from capstone import *
 
+class Addresses:
+    data = b"" # data
+    def __init__(self, data):
+        self.data = data
+
+    def _get_jmp_call(self, p, end_ptr):
+        rel32 = None
+        is_rip_relative = False
+        if (p + 5 <= end_ptr):
+            if data[p] == 0xE8 or data[p] == 0xE9:
+                rel32 = data[p+1:p+5] # TODO
+        
+        if (p + 6 <= end_ptr):
+            if data[p] == 0x0F and (data[p+1] & 0xF0) == 0x80:
+                if data[p+1] != 0x8A and data[p+1] != 0x8B: 
+                    rel32 = data[p+2:p+6]
+                    # not JPE / JPO
+            else if ((data[p] == 0xFF and (data[p+1] in [0x15, 0x25])) or data[p] in (0x89, 0x8B, 0x8D) and (data[p+1] & 0xC7 == 0x05):
+                rel32 = data[p+2:p+6]
+                is_rip_relative = True
+        
+        if (p + 7 <= end_ptr):
+            if (data[p] & 0xF2) == 0x40 or data[p] == 0x66) and (p[1] in [0x89, 0x8B, 0x8D]) and (p[2] & 0xC7 == 0x05):
+                rel32 = data[p+3:p+7]
+                is_rip_relative = True
+        
+        return rel32
+
+    def _treat_rel32(self, rel32, adjust_pointer_to_rva):
+        if not rel32:
+            return None
+        
+        #  TODO adjust_pointer_to_rva
+        #rel32_rva = rel32 - adjust_pointer_to_rva
+        # is there an abs32 reloc, overlapping the candidate?
+        return rel32
+
+    def get_rel32(self):
+        p = 0 # start_offset
+        while p < end_pointer:
+            pass
+    pass
+
 class Disassembler:
     def __init__(self, fname):
         self.fname = fname
@@ -16,6 +59,18 @@ class Disassembler:
         # RVA と File Offset を見てなおす
         pass
 
+    def check_section(rva):
+        file_offset = rva_to_file_offset(rva)
+        # TODO sections
+        for x in sections:
+            section_header = SectionHeader(section_id)
+            start_offset = section_header.sh_offset
+            end_offset = start_offset + section_header.sh_size - 5 + 1
+
+    def parse_progbits():
+        emit_origin(origin)
+        next_relocation = section_end
+        #if (current_abs_offset != end_abs_offset and next_relocation > current
     
     def disassemble(self):
         f = open(self.fname, "rb")
@@ -37,3 +92,15 @@ class Disassembler:
             instrs_all.append(instrs)
 
         return instrs_all
+
+    def parse_file(program, receptor):
+        file_offset = 0
+        abs_offsets = []
+        abs32_locations_ = []
+
+        # rvvs_to_file_offsets
+        # 各セクションヘッダを見つつ
+        if section.section_type == SHT_REL:
+            pass
+        if section.section_type == SHT_PROGBITS:
+            pass
