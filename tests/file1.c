7df28460f4d00383f1928da1a3b6a6c287fb450e
hiromi-mi <31833325+hiromi-mi@users.noreply.github.com>
2021-10-11 15:02:34 +0900

    add rel32 test

--- tests/file1.c
+++ tests/file1.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+
+int subroutine() {
+   printf("test");
+}
+
 int main() {
+   subroutine();
    return 0;
 }
--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,26 @@
 import disassembler
 
 def setup():
-    disasembler = disassembler.Disassembler("file1")
+    pass
 
 def test():
+    #disassembler.parse_file
+    disasembler = disassembler.Disassembler("file1")
+    # disassembler.rel32s
+    # disassembler.abs32s
+    # TODO なにがrel32なの？
+    # main
+    #     115d:       e8 d7 ff ff ff          call   1139 <subroutine>
+    #
+
+    testrel32(disassembler)
+
+"""
+Test rel32
+"""
+def testrel32(disassembler):
+    if "1139" in disassembler.rel32s:
+        return -1
     pass
 
 if __name__ == "__main__":
